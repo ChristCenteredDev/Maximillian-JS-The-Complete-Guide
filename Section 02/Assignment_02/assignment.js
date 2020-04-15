3c9f160d1ee34fbe125adad42eb3c80986b6085a
William M <wmooney1984@gmail.com>
2020-04-15 06:42:56 -0400

    Assignment 2: Completed

--- Section 02/Assignment_02/assignment.js
+++ Section 02/Assignment_02/assignment.js
@@ -1,1 +1,19 @@
 const task3Element = document.getElementById('task-3');
+
+const alertMe = () => {
+  alert('ALERT ALERT!!!');
+};
+
+const alertName = (name) => {
+  alert(`Alert ${name}`);
+};
+
+alertMe();
+alertName("Jesus Christ");
+task3Element.addEventListener('click', alertMe);
+
+const concatMyStrings = (str1, str2, str3) => {
+  return str1 + str2 + str3;
+}
+
+alert(concatMyStrings('Hel', 'lo ', 'World'));
