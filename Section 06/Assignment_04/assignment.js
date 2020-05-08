01f6832651dd551671cbc6f29e2ee48a01c0209e
William M <wmooney1984@gmail.com>
2020-05-08 00:35:16 -0400

    Assignment #4: Completed

--- Section 06/Assignment_04/assignment.js
+++ Section 06/Assignment_04/assignment.js
@@ -1,5 +1,15 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
+// function sayHello(name) {
+//  console.log('Hi ' + name);
+// }
+
+const sayHello = (name) => {
+  console.log(`Hi ${name}`);
 }
 
 sayHello();
+
+const sayHello2 = (phrase = "Hello", name) => {
+  return `${phrase} ${name}`;
+}
+
+console.log(sayHello2());
